a706dada7f92f550eb18f6beccba753efcd2d191
ceeygithub <cynthiaekhomwandolor@gmail.com>
2024-03-29 12:16:31 +0100

    responsive hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,37 +1,24 @@
-
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import HeroCss from '../styles/Hero.module.css'
+import HeroCss from '../styles/Hero.module.css';
 
 const Hero = () => {
- // Get the history object from React Router
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const handleGetStarted = () => {
-    // Redirect to the sign-up page
-   
-     navigate('./signUp');
-  };
+    const handleGetStarted = () => {
+        navigate('./signUp');
+    };
 
-  return (
-  
-    <>
-       <div className={ HeroCss.heroSection}>
-
-        <div className={HeroCss.heroContent}>
-       
+    return (
+        <div className={HeroCss.heroSection}>
+            <div className={HeroCss.heroContent}>
                 <h1>Convene - Where Questions Meet Answers</h1>
-                 <p> Convene empowers you to crowdsource questions for meetups and
-                events.
-            </p>
-         <p>Join the conversation, shape the agenda.</p>
-              <button className={HeroCss.ctaButton} onClick={ handleGetStarted}>Get Started</button> 
+                <p>Convene empowers you to crowdsource questions for meetups and events.</p>
+                <p>Join the conversation, shape the agenda.</p>
+                <button className={HeroCss.ctaButton} onClick={handleGetStarted}>Get Started</button>
+            </div>
         </div>
-         
-    </div>
- 
-    </>
-  );
+    );
 };
 
 export default Hero;
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,17 +1,80 @@
 
 
-import React from 'react';
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+// import NavCSS from '../styles/Navigation.module.css';
+// import logoImg from '../assets/Group 1.png';
+// import DropdownAvatar from './DropdownAvatar';
+// import SearchBar from './SearchBar';
+// import { IoMdNotificationsOutline } from 'react-icons/io';
+// import { FiMessageSquare } from 'react-icons/fi';
+// import { useAuth } from '../contexts/AuthContext'; 
+
+// const Navigation = () => {
+//   const {  isAuthenticated } = useAuth(); 
+
+//   const handleSearch = () => {
+//     // Implement your search logic here
+//     console.log('Search');
+//   };
+
+//   return (
+//     <nav className={NavCSS.navbar}>
+//       <div className={NavCSS.navbarContainer}>
+//         <Link to="/" className={NavCSS.navbarLogo}>
+//           <img src={logoImg} alt="" className={NavCSS.logoImg} />
+//         </Link>
+//         {isAuthenticated() && (
+//           <>
+//             <SearchBar onChange={handleSearch} />
+//             <div className={NavCSS.navbarLinks}>
+//               <Link to="/explore" className={NavCSS.navbarLink}>
+//                 <FiMessageSquare />
+//                 Messages
+//               </Link>
+//               <Link to="/notifications" className={NavCSS.navbarLink}>
+//                 <IoMdNotificationsOutline />
+//                 Notifications
+//               </Link>
+                 
+//               <DropdownAvatar onCloseDropdown={() => {}} />
+       
+//             </div>
+//           </>
+//         )}
+//         {!isAuthenticated() && (
+//           <div className={NavCSS.navbarLinks}>
+//             <Link to="/signup" className={`${NavCSS.navbarLink} ${NavCSS.signup}`}>
+//               Sign Up
+//             </Link>
+//             <Link to="/signin" className={`${NavCSS.navbarLink} ${NavCSS.signin}`}>
+//               Sign In
+//             </Link>
+//           </div>
+//         )}
+//       </div>
+//     </nav>
+//   );
+// };
+
+// export default Navigation;
+
+
+
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import NavCSS from '../styles/Navigation.module.css';
 import logoImg from '../assets/Group 1.png';
 import DropdownAvatar from './DropdownAvatar';
 import SearchBar from './SearchBar';
 import { IoMdNotificationsOutline } from 'react-icons/io';
+import { IoIosMenu } from "react-icons/io";
 import { FiMessageSquare } from 'react-icons/fi';
 import { useAuth } from '../contexts/AuthContext'; 
 
 const Navigation = () => {
-  const {  isAuthenticated } = useAuth(); 
+  const { isAuthenticated } = useAuth(); 
+  const [showMenu, setShowMenu] = useState(false);
 
   const handleSearch = () => {
     // Implement your search logic here
@@ -24,10 +87,13 @@ const Navigation = () => {
         <Link to="/" className={NavCSS.navbarLogo}>
           <img src={logoImg} alt="" className={NavCSS.logoImg} />
         </Link>
-        {isAuthenticated() && (
-          <>
-            <SearchBar onChange={handleSearch} />
-            <div className={NavCSS.navbarLinks}>
+        <div className={NavCSS.mobileMenuIcon} onClick={() => setShowMenu(!showMenu)}>
+          <IoIosMenu />
+        </div>
+        <div className={`${NavCSS.navbarLinks} ${showMenu ? NavCSS.showMenu : ''}`}>
+          {isAuthenticated() && (
+            <>
+              <SearchBar onChange={handleSearch} />
               <Link to="/explore" className={NavCSS.navbarLink}>
                 <FiMessageSquare />
                 Messages
@@ -36,26 +102,23 @@ const Navigation = () => {
                 <IoMdNotificationsOutline />
                 Notifications
               </Link>
-                 
               <DropdownAvatar onCloseDropdown={() => {}} />
-       
+            </>
+          )}
+          {!isAuthenticated() && (
+            <div className={NavCSS.authLinks}>
+              <Link to="/signup" className={`${NavCSS.navbarLink} ${NavCSS.signup}`}>
+                Sign Up
+              </Link>
+              <Link to="/signin" className={`${NavCSS.navbarLink} ${NavCSS.signin}`}>
+                Sign In
+              </Link>
             </div>
-          </>
-        )}
-        {!isAuthenticated() && (
-          <div className={NavCSS.navbarLinks}>
-            <Link to="/signup" className={`${NavCSS.navbarLink} ${NavCSS.signup}`}>
-              Sign Up
-            </Link>
-            <Link to="/signin" className={`${NavCSS.navbarLink} ${NavCSS.signin}`}>
-              Sign In
-            </Link>
-          </div>
-        )}
+          )}
+        </div>
       </div>
     </nav>
   );
 };
 
 export default Navigation;
-
